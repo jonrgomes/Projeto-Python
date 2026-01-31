be8c26a71aa6c569c00b22fffe8434a4c48708d4
Jonatas Gomes <jonrgomes@gmail.com>
2026-01-30 21:43:18 -0300

    add primeiro ex003 em python

---
+++ ex003.py
@@ -0,0 +1,18 @@
+n1=int(input('Digite um valor:'))
+n2=int(input('Digite outro valor:'))
+s=n1+n2
+print('A soma entre {} e {} é igual a {}!'.format(n1,n2,s))
+
+n3=float(input('Qual o seu peso:'))
+n4=float(input('Qual a sua altura:'))
+t=n3+n4
+print('A soma entre {} e {} é {},'.format(n3,n4,t))
+
+
+
+
+
+
+
+
+
