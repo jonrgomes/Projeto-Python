a6004697b2985e6a604ef43ce1006814d150c748
Jonatas Gomes <jonrgomes@gmail.com>
2026-02-05 21:07:08 -0300

    ex011

---
+++ ex011.py
@@ -0,0 +1,7 @@
+largura=float(input('Qual a largura da parede?'))
+altura=float(input('Qual a altura da parede?'))
+area=largura*altura
+print('A área da parede é {:.2f}m²'.format(area))
+tinta=area/5
+print('Utilizaremos {:.2f}l de tinta nesta área!'.format(tinta))
+
