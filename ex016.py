2e844c36719a8be178a3cede33003a919deee370
Jonatas Gomes <jonrgomes@gmail.com>
2026-02-08 21:11:14 -0300

    Curso_em_video_Aula008

---
+++ ex016.py
@@ -0,0 +1,6 @@
+from math import trunc
+vlr=float(input('Digite um valor:'))
+print('O valor digitado foi {} e a sua porção inteira é {}!'.format(vlr,trunc(vlr)))
+#OU
+num=float(input('Digite um valor:'))
+print('O valor digitado foi {} e sau porção interia é {}!'.format(num, int(num)))
