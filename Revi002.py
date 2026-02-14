356353869f1ba78e15c3c928e5f4356ba807553e
jonrgomes <jonrgomes@gmail.com>
2026-02-13 21:46:02 -0300

    Revisando_o_conteudo

---
+++ Revi002.py
@@ -0,0 +1,10 @@
+frase = 'Curso em vídeo Python'
+print(frase[3])
+print(frase[:13])
+print(frase[5:9])
+print(frase[::2])
+print(frase.count('e'))
+print(frase.find('deo'))
+print(len(frase))
+print(frase.split())
+
