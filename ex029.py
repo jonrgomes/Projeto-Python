519e75e50afee94dc9944fb56edcd7a87520702b
jonrgomes <jonrgomes@gmail.com>
2026-02-20 20:03:21 -0300

    Curso_em_video_Aula010

--- ex029.py
+++
@@ -1,6 +0,0 @@
-velocidade = int(input('Qual a velocidade do veículo:?'))
-if velocidade <=80:
-    print('Parabéns, você está dentro da velocidade permitida!')
-else:
-    print('MULTADO! Você excedeu o limete de velociade permitida na via!')
-
---
+++ ex030.py
@@ -0,0 +1,6 @@
+n = int(input('Me diga um número qualquer:'))
+if n % 2 == 0:
+    print('Esse número é par!')
+else:
+    print('Esse número é impar!')
+
